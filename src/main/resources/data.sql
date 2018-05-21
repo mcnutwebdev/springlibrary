8c5ebfbd3a003599da3ddb89fe75ff2ed9fafd29
mcnutwebdev <mcnutwebdev@gmail.com>
2018-05-21 21:47:04 +0100

    Initial commit - Project set up

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into customers (CID, CNAME) values (1, "John");
+insert into customers (CID, CNAME) values (2, "Tom");
+insert into customers (CID, CNAME) values (3, "Fred");
+insert into books (BID, TITLE, AUTHOR) values (100, "The Da Vinci Code", "Dan Brown");
+insert into books (BID, TITLE, AUTHOR) values (101, "The Boy in the Striped Pyjamas", "John Boyne");
+insert into books (BID, TITLE, AUTHOR) values (102, "Mayday", "Thomas Block");
+insert into books (BID, TITLE, AUTHOR) values (103, "Runaway Jury", "John Grisham");
+insert into books (BID, TITLE, AUTHOR) values (104, "Big Maggie", "John B. Keane");
+insert into loans (LID, BID, CID, DUEDATE) values (2000, 100, 2, "2017-10-04");
+insert into loans (LID, BID, CID, DUEDATE) values (2001, 101, 2, "2017-11-01");
+insert into loans (LID, BID, CID, DUEDATE) values (2002, 104, 2, "2017-11-04");
+insert into loans (LID, BID, CID, DUEDATE) values (2003, 103, 3, "2017-11-27");
